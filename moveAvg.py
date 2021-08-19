96e8902ccef30adb20d751c8fb702cb2f73fe430
Paras5338 <paraschauhan57043@gmail.com>
2021-08-19 23:47:13 +0530

    code updated

--- moveAvg.py
+++ moveAvg.py
@@ -2,42 +2,42 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use('fivethirtyeight')
+# plt.style.use('fivethirtyeight')
 
 #load the data
-Cisco = pd.read_csv('cisco.csv')
+Congnizant = pd.read_csv('CTSH.csv')
 
-#print(Cisco)
+#print(Congnizant)
 #visualize the data
 #plt.figure(figsize=(12.5,5.5))
-#plt.plot(Cisco['Close'], label='Cisco Historical Price')
-#plt.plot(Cisco['Adj Close'], label='Volume of shares traded')
-#plt.title('Cisco Close Price Chart')
-#plt.xlabel('26 Mar, 1990 - 20 May, 2021')
+#plt.plot(Congnizant['Close'], label='Congnizant Historical Price')
+#plt.plot(Congnizant['Adj Close'], label='Volume of shares traded')
+#plt.title('Congnizant Close Price Chart')
+#plt.xlabel('19 Jun, 1998 - 19 Aug, 2021')
 #plt.ylabel('Close price in rupees')
 #plt.legend(loc='upper left')
 #plt.show()
 
 #create the simple moving average with a 30 days window
 SMA30 = pd.DataFrame()
-SMA30['Close'] = Cisco['Close'].rolling(window=30).mean()
+SMA30['Close'] = Congnizant['Close'].rolling(window=30).mean()
 SMA100 = pd.DataFrame()
-SMA100['Close'] = Cisco['Close'].rolling(window=100).mean()
+SMA100['Close'] = Congnizant['Close'].rolling(window=100).mean()
 
-#visualize the SMA30, SMA100 and Cisco close price
+#visualize the SMA30, SMA100 and Congnizant close price
 #plt.figure(figsize=(12.5,5.5))
-#plt.plot(Cisco['Close'], label='Cisco Close Price')
+#plt.plot(Congnizant['Close'], label='Congnizant Close Price')
 #plt.plot(SMA30['Close'], label='SMA30')
 #plt.plot(SMA100['Close'], label='SMA100')
-#plt.title('Coparision of Cisco SMA30, SMA100 and Close Price')
-#plt.xlabel('26 Mar, 1990 - 20 May, 2021')
+#plt.title('Coparision of Congnizant SMA30, SMA100 and Close Price')
+#plt.xlabel('19 Jun, 1998 - 19 Aug, 2021')
 #plt.ylabel('Close Price')
 #plt.legend(loc='upper left')
 #plt.show()
 
 #create a new data frame to store all the Data
 data = pd.DataFrame()
-data['Cisco'] = Cisco['Close']
+data['Congnizant'] = Congnizant['Close']
 data['SMA30'] = SMA30['Close']
 data['SMA100'] = SMA100['Close']
 #print(data)
@@ -50,7 +50,7 @@ def buy_sell(data):
     for i in range(len(data)):
         if data['SMA30'][i] > data['SMA100'][i]:
             if flag != 1:
-                sigPriceBuy.append(data['Cisco'][i])
+                sigPriceBuy.append(data['Congnizant'][i])
                 sigPriceSell.append(np.nan)
                 flag=1;
             else:
@@ -59,7 +59,7 @@ def buy_sell(data):
         elif data['SMA30'][i] < data['SMA100'][i]:
             if flag != 0:
                 sigPriceBuy.append(np.nan)
-                sigPriceSell.append(data['Cisco'][i])
+                sigPriceSell.append(data['Congnizant'][i])
                 flag = 0
             else:
                 sigPriceBuy.append(np.nan)
@@ -79,13 +79,13 @@ data['Sell_Signal_Price'] = buy_sell_data[1]
 
 #visualize the data and market trend of buy and sell
 plt.figure(figsize=(12.5,5.5))
-plt.plot(data['Cisco'], label='Cisco Close Price', alpha=0.5)
+plt.plot(data['Congnizant'], label='Congnizant Close Price', alpha=0.5)
 plt.plot(data['SMA30'], label ='SMA30', alpha=0.5)
 plt.plot(data['SMA100'], label ='SMA100', alpha=0.5)
 plt.scatter(data.index, data['Buy_Signal_Price'], label = 'Buy', marker = '^', color='green')
 plt.scatter(data.index, data['Sell_Signal_Price'], label = 'Sell', marker = 'v', color='red')
-plt.title('Cisco Close price history of buy and sell')
-plt.xlabel('26 Mar, 1990 - 20 May, 2021')
+plt.title('Congnizant Close price history of buy and sell')
+plt.xlabel('19 Jun, 1998 - 19 Aug, 2021')
 plt.ylabel('Close Price')
 plt.legend(loc='upper left')
 plt.show()
